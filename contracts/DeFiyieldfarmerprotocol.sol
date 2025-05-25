14367a3f2a85379757317cbb085aa822606cade6
loalitachoudhary-123 <lalitachoudhary9754@gmail.com>
2025-05-25 23:42:38 +0530

    Update DeFiyieldfarmerprotocol.sol

--- contracts/DeFiyieldfarmerprotocol.sol
+++ contracts/DeFiyieldfarmerprotocol.sol
@@ -5,9 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-/**
- * @title Projec
- * @dev A DeFi yield farming contract where users can stake tokens and earn rewards
+
  */
 contract Project is Ownable, ReentrancyGuard {
     IERC20 public stakingToken;
